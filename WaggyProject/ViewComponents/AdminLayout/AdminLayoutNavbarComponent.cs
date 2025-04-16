a16e42b1127221fe99532fd728f5e3141599f96d
Erhan Gündüz <erhangndz@live.com>
2025-04-16 21:11:17 +0300

    updated

---
+++ WaggyProject/ViewComponents/AdminLayout/AdminLayoutNavbarComponent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WaggyProject.Entities;
+
+namespace WaggyProject.ViewComponents.AdminLayout
+{
+    public class AdminLayoutNavbarComponent: ViewComponent
+    {
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdminLayoutNavbarComponent(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+           var userName =  User.Identity.Name;
+
+            var user = await _userManager.FindByNameAsync(userName);
+
+            ViewBag.fullName = String.Join(" ", user.FirstName, user.LastName);
+            ViewBag.userName = userName;
+            return View();
+        }
+    }
+}
